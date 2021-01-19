e68eb31709e7c735af6d7a6c92fee7adf2c1ae7b
Osanda <osanda.v13@gmail.com>
2021-01-19 11:06:02 +0530

    initial commit

---
+++ src/test/java/com/osanda/premier/league/PremierLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osanda.premier.league;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremierLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
