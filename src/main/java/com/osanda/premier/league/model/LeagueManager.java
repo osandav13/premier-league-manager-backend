de9d9aa27861b3453cddeeb10c992b852411ef13
Osanda <osanda.v13@gmail.com>
2021-01-21 12:45:28 +0530

    LeagueManager interface created

--- src/main/java/com/osanda/premier/league/model/LeagueManager.java
+++ src/main/java/com/osanda/premier/league/model/LeagueManager.java
@@ -1,2 +1,18 @@
-package com.osanda.premier.league.model;public interface LeagueManager {
+package com.osanda.premier.league.model;
+
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**  this interface provide base implementation for PremierLeagueManager
+ * @author Osanda Ginige w1761754
+ */
+public interface LeagueManager {
+    void createFootballClub(FootballClub club);
+    void deleteClub(String clubName);
+    void displayStatistics(String clubName);
+    void displayLeagueTable();
+    void addMatchDetails(Match match);
+    void saveToFile() throws FileNotFoundException, IOException;
+    void retrieveFromFile() throws IOException, FileNotFoundException, EOFException, ClassNotFoundException;
 }
