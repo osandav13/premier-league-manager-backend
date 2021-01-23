4cd321dc675913e73e7417059aa67f5fee045bb6
Osanda <osanda.v13@gmail.com>
2021-01-23 10:01:40 +0530

    deleteClub and displayStatistics implemented

--- src/main/java/com/osanda/premier/league/service/PremierLeagueManager.java
+++ src/main/java/com/osanda/premier/league/service/PremierLeagueManager.java
@@ -36,14 +36,47 @@ public class PremierLeagueManager implements LeagueManager {
         System.out.println(club.getClubName() + " in" + club.getLocation() + " was added to the Premier League");
     }
 
+    /**
+     * Removes a football club instance from the leagueTeams List
+     * If the removal is successful name of the club will be displayed
+     *
+     * @param clubName name of the football club element to be removed
+     */
     @Override
-    public void deleteClub(String clubName) {
-
+    public void deleteClub(String clubName){
+        for (SportsClub club : leagueTeams){
+            if (club.getClubName().equals(clubName)){
+                leagueTeams.remove(club);
+                System.out.println(club.getClubName() + " has been removed from Premier league");
+                return;
+            }
+        }
+        System.out.printf("Football club named %s not in Premier League\n",clubName);
     }
 
+    /**
+     * Display statistics about football club from the LeagueTeams List
+     * If the provided club is a element of leagueTeams message will be displayed
+     *
+     * @param clubName name of the club element to display the information
+     */
     @Override
     public void displayStatistics(String clubName) {
-
+        for (FootballClub club : leagueTeams){
+            if (club.getClubName().equals(clubName)){
+                System.out.println("Club name :- " + club.getClubName());
+                System.out.println("Club location :- " + club.getLocation());
+                System.out.println("Wins :- " + club.getNumberOfWins());
+                System.out.println("Defeats :- " + club.getNumberOfDefeats());
+                System.out.println("Draws :- " + club.getNumberOfDraws());
+                System.out.println("Points :- " + club.getNumberOfPoints());
+                System.out.println("Goals scored :- " + club.getNumberOfGoalsScored());
+                System.out.println("Goals received :- " + club.getNumberOfGoalsReceived());
+                System.out.println("Matches played :- " + club.getNumberOfMatchesPlayed());
+                return;
+            }
+        }
+        System.out.printf("%s is not a Premier league club\n",clubName);
     }
 
     @Override
