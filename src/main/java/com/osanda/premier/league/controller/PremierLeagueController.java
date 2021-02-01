262e2cb7525eac9017aa4dc277afebc655c40be6
Osanda <osanda.v13@gmail.com>
2021-02-02 00:35:47 +0530

    REST api matchHistory endpoint implemented

--- src/main/java/com/osanda/premier/league/controller/PremierLeagueController.java
+++ src/main/java/com/osanda/premier/league/controller/PremierLeagueController.java
@@ -1,9 +1,11 @@
 package com.osanda.premier.league.controller;
 
 import com.osanda.premier.league.model.FootballClub;
+import com.osanda.premier.league.model.Match;
 import com.osanda.premier.league.service.PremierLeagueManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -26,5 +28,28 @@ public class PremierLeagueController {
     @GetMapping("/api/leagueTable")
     public List<FootballClub> getLeague() {
         return premierLeagueManager.getLeagueTeams();
+    }
+
+    /**
+     * GET request to /api/matchHistory are served
+     * depending on the parameter response differ
+     * @param date a request String date is passed
+     * @return List of Match JASON objects are returned
+     */
+    @CrossOrigin
+    @GetMapping("/api/matchHistory")
+    public List<Match> getMatchHistory(@RequestParam(required = false) String date){
+        if (date==null){
+            return premierLeagueManager.getMatchHistory();
+        }else {
+            List<Match> matches = new ArrayList<>();
+            for (Match match:premierLeagueManager.getMatchHistory()){
+                if (match.getMatchDate().toString().contains(date)){
+                    matches.add(match);
+                }
+            }
+            return matches;
+        }
+
     }
 }
